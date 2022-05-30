440d455955d1f1d55bfa224b3738613dae84c180
Marat Isaev <fake@mail.com>
2022-05-30 22:32:50 +0600

    remove redundant files

--- src/test.js
+++
@@ -1,8 +0,0 @@
-function shuffle(array) {
-    for (let j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x) ;
-    return array;
-}
-
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-console.log(shuffle(a))
