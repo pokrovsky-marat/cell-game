2e7b16bae8098a2e397d2aca9aff7d51fbcea270
Marat Isaev <fake@mail.com>
2022-05-31 00:53:10 +0600

    Modified App.js to work with any amount of cells

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -8,20 +8,19 @@ import {GameResult} from '../GameResult/GameResult';
 
 
 const App = observer(() => {
-        //Все клетки открыты и игра началась, значит игра закончена
         let isGameOver = (store.cells.length === store.cells.filter(item => item.isClicked).length) &&
             store.isGameStarted;
+        //Делим массив на подмассивы в зависимости от уровня игры и мапимся по полученному массиву массивов
+        let countRows = store.settings[store.level].rows;
+        let cellRows = chunkArray(store.cells, countRows).map((row, idx) => (<RenderTableRow
+            key={idx}
+            row={row}/>));
         return (
             <div>
                 <div className={'text-center mt-3 mb-3'}>
                     {store.isGameStarted ? '' : <StartGameButton/>}
                 </div>
-
-                {/*Перед тем как мапить массив, делим его на 4 под массива*/}
-                {chunkArray(store.cells, 4).map((row, idx) => (<RenderTableRow
-                    key={idx}
-                    row={row}/>))}
-
+                {cellRows}
                 {isGameOver ?
                     <div className={'text-center fs-5 mt-3'}>
                         <GameResult/>
