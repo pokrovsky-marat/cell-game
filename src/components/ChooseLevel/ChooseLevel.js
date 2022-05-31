6300c5c1fa355d84906b6573ed7d9c3f2707e5e5
Marat Isaev <fake@mail.com>
2022-05-31 13:30:21 +0600

    Minor changes to skip warnings

--- src/components/ChooseLevel/ChooseLevel.js
+++ src/components/ChooseLevel/ChooseLevel.js
@@ -5,10 +5,11 @@ let ChooseLevel = () => {
         store.changeLevel(e.target.value);
     };
     return (
-        <div className="d-flex justify-content-center mt-3" onChange={onChangeValue}>
+        <div className="d-flex justify-content-center mt-3">
             {store.settings.map((item, idx) => (
-                <div className="form-check me-3">
-                    <input className="form-check-input" checked={idx == store.level} type="radio" value={idx}
+                <div key={idx} className="form-check me-3">
+                    <input className="form-check-input" onChange={onChangeValue} checked={+idx === +store.level}
+                           type="radio" value={idx}
                            name="level" id={`radioBtn${idx}`}/>
                     <label className="form-check-label" htmlFor={`radioBtn${idx}`}> {item.description} </label>
                 </div>
